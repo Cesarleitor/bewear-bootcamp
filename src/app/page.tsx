03b470f6aa936439f9892a7103fa2e150a3e23dd
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-08-09 16:06:20 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,61 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
+
+import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/Footer";
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
 import { productTable } from "@/db/schema";
-import Product from "./product/page";
-import ProductList from "@/components/common/product-list";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
-    }
+    },
   });
-  console.log(products);
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
+  const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
-      <div className="space-y-6 px-5">
-        <Image
-          src="/banner01.png"
-          alt="Leve uma vida com estilo"
-          height={0}
-          width={0}
-          sizes="100vn"
-          className="h-auto w-full"
-        />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
 
-        <ProductList products={products} title="Mais Vendidos"/>
+        <ProductList products={products} title="Mais vendidos" />
 
-        <Image
-          src="/banner02.png"
-          alt="Leve uma vida com estilo"
-          height={0}
-          width={0}
-          sizes="100vn"
-          className="h-auto w-full"
-        />
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
